a35b28df2a379bc33f2d3ddb6ac83a6f8f0cc3f6
zhangyongwang <20225614@qq.com>
2019-02-17 11:11:56 +0800

    两数之和等于目标值，返回下标 用 HashMap实现

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,41 +1,49 @@
 package com.company;
 
+import com.company.demo.TwoSums;
+
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // 集合
+//        // 集合
+//
+//        //列表
+//        List<String> arrayList = new ArrayList<>();
+//        List<String> linkedList = new LinkedList<>();
+//
+//        // homework : 理解 linkedList
+//
+//        arrayList.add("test");
+//        arrayList.get(0);
+//        arrayList.subList(0, 0);
+//
+//        // for 循环 遍历 列表
+//        for (String item : arrayList) {
+//            System.out.println(item);
+//        }
+//
+//        // Set -- 主要用于 去重操作
+//        Set<String> set = new HashSet<>();
+//
+//        set.add("abc");
+//        set.add("alex");
+//        set.add("john");
+//        set.add("alex");
+//        set.add("abc");
+//
+//        for (String item : set) {
+//            System.out.println(item);
+//        }
 
-        //列表
-        List<String> arrayList = new ArrayList<>();
-        List<String> linkedList = new LinkedList<>();
 
-        // homework : 理解 linkedList
+        // TwoSums 的测试用例
+        TwoSums test = new TwoSums();
 
-        arrayList.add("test");
-        arrayList.get(0);
-        arrayList.subList(0, 0);
-
-        // for 循环 遍历 列表
-        for (String item : arrayList) {
-            System.out.println(item);
-        }
-
-        // Set -- 主要用于 去重操作
-        Set<String> set = new HashSet<>();
-
-        set.add("abc");
-        set.add("alex");
-        set.add("john");
-        set.add("alex");
-        set.add("abc");
-
-        for (String item : set) {
-            System.out.println(item);
-        }
-
+        test.twoSums(new ArrayList<>(Arrays.asList(1, 2, 3, 5, 8, 7)), 9);
 
     }
 }
--- src/com/company/demo/TwoSums.java
+++ src/com/company/demo/TwoSums.java
@@ -1,27 +1,28 @@
 package com.company.demo;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class TwoSums {
-    ArrayList<Integer> twoSums(ArrayList<Integer> nums, Integer target) {
+    public ArrayList<Integer> twoSums(ArrayList<Integer> nums, Integer target) {
 
-        // 出现过什么样的数字,可能有重复
-        Set<Integer> numSet = new HashSet<>();
+        // 出现过什么样的数字为 key ,他的原来位置为value
+        Map<Integer, Integer> numMap = new HashMap<>();
 
-        for (Integer num : nums) {
-            numSet.add(num);
+        for (int i = 0; i < nums.size(); i++) {
+            numMap.put(nums.get(i), i);
         }
 
-        //找到一对数的和 == target
-        for (Integer num : nums) {
-            if (numSet.contains(target - num)) {
+
+        //找到一对数的和 == target!!
+        for (int j = 0; j < nums.size(); j++) {
+            if (numMap.containsKey(target - nums.get(j))) {
                 ArrayList<Integer> result = new ArrayList<>();
-                result.add(num);
-                result.add(target - num);
+                result.add(j);
+                result.add(numMap.get(target - nums.get(j)));
+                System.out.println(result);
                 return result;
             }
         }
+        return null;
     }
 }
